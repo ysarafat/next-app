5dfe6f1835d320a615fa51878ebfe0e59c2556fe
Yeasir Arafat <ysarafat.bd@gmail.com>
2024-11-29 11:56:53 +0600

    Refactor homepage content and remove Next.js logo image

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,10 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+   
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by Arafat{" "}
+            Get started by Arafat{
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               app/page.tsx
             </code>
